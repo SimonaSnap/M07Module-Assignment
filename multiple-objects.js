1b33c834ea912d659ab7efe85f439d6e2aac8264
Simona <simona_s@outlook.com>
2023-02-10 13:24:46 -0800

    script work

--- multiple-objects.js
+++ multiple-objects.js
@@ -11,22 +11,28 @@ function Hotel(name, rooms, booked) {
   
   // Create three hotel objects
   var quayHotel = new Hotel('Quay', 40, 25);
-  var parkHotel = new Hotel('Park', 120, 77);
+  var sunHotel = new Hotel('Park', 120, 77);
   var sunHotel = new Hotel("Sunset", 86, 10)
   
   
   // Update the HTML for the page
   var details1 = quayHotel.name + ' rooms: ';
       details1 += quayHotel.checkAvailability();
+      var hotelName1 = document.createElement("h3");
+      hotelName1.textContent = details1
   var elHotel1 = document.getElementById('hotel1');
-  elHotel1.textContent = details1;
-  
+  elHotel1.appendChild(hotelName1);
+
   var details2 = parkHotel.name + ' rooms: ';
       details2 += parkHotel.checkAvailability();
+      var hotelName2 = document.createElement("h3");
+      hotelName2.textContent = details2
   var elHotel2 = document.getElementById('hotel2');
-  elHotel2.textContent = details2;
+  elHotel2.appendChild(hotelName2);
 
   var details3 = sunHotel.name + ' rooms: ';
       details3 += sunHotel.checkAvailability();
+      var hotelName3 = document.createElement("h3");
+      hotelName3.textContent = details3
   var elHotel3 = document.getElementById('hotel3');
-  elHotel3.textContent = details3;
+  elHotel3.appendChild(hotelName3);
